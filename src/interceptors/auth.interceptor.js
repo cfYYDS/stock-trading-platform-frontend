19d50d3a3ef783df9c821baed627957822cd23a9
WillW404 <will.wang@beaconfireinc.com>
2024-05-23 17:01:37 -0400

    Starter

--- src/interceptors/auth.interceptor.js
+++ src/interceptors/auth.interceptor.js
@@ -1,18 +1,13 @@
 import axios from 'axios';
 
-axios.interceptors.request.use(
-  (config) => {
-    if (import.meta.env.PROD) {
-      config.url = config.url.replace('/api', import.meta.env.VITE_API_URL);
-    }
-    config.headers.Authorization = `Bearer ${
-      localStorage.getItem('token') || sessionStorage.getItem('token')
-    }`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+axios.interceptors.request.use((config) => {
+  if (import.meta.env.PROD) {
+    config.url = config.url.replace('/api', import.meta.env.VITE_API_URL);
+  }
+  config.headers.Authorization = `Bearer ${
+    localStorage.getItem('token') || sessionStorage.getItem('token')
+  }`;
+  return config;
+});
 
 export default axios;
